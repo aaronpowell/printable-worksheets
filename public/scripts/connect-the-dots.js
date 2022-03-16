f383c88fb9e5976a883659e4e0362ed66e6239b4
Aaron Powell <me@aaron-powell.com>
2022-03-17 10:07:59 +1100

    Adding in all images

--- public/scripts/connect-the-dots.js
+++ public/scripts/connect-the-dots.js
@@ -1,9 +1,9 @@
 const files = [
   { label: "✏ Bit's first day at school", path: "Connect the dots 1.svg" },
-  // { label: "✏ Connecet the dots #2", path: "Connect the dots 2.svg" },
-  // { label: "✏ Connecet the dots #3", path: "Connect the dots 3.svg" },
-  // { label: "✏ Connecet the dots #4", path: "Connect the dots 4.svg" },
-  // { label: "✏ Connecet the dots #5", path: "Connect the dots 5.svg" },
+  { label: "✏ Who's Bit's friend", path: "Connect the dots 2.svg" },
+  { label: "✏ Bit and Java Duke", path: "Connect the dots 3.svg" },
+  { label: "✏ Make Bit's laptop", path: "Connect the dots 4.svg" },
+  { label: "✏ Bit learns to code", path: "Connect the dots 5.svg" },
 ].map((f) => ({ ...f, path: `images/connect-the-dots/${f.path}` }));
 
 const loadFile = (file) => {
--- public/scripts/differences.js
+++ public/scripts/differences.js
@@ -1,9 +1,9 @@
 const files = [
   { label: "🔎 Bit and T-Rex", path: "Circle the Difference 1.svg" },
-  // { label: "🔎 Find the differences #2", path: "Circle the Difference 2.svg" },
-  // { label: "🔎 Find the differences #3", path: "Circle the Difference 3.svg" },
-  // { label: "🔎 Find the differences #4", path: "Circle the Difference 4.svg" },
-  // { label: "🔎 Find the differences #5", path: "Circle the Difference 5.svg" },
+  { label: "🔎 Bit and Xamarin", path: "Circle the Difference 2.svg" },
+  { label: "🔎 Bit and friends play sport", path: "Circle the Difference 3.svg" },
+  { label: "🔎 Bit at a user group", path: "Circle the Difference 4.svg" },
+  { label: "🔎 Bit and Clippy", path: "Circle the Difference 5.svg" },
 ].map((f) => ({ ...f, path: `images/differences/${f.path}` }));
 
 const loadFile = (file) => {
